3a828088dd33512efa67e4c50ec9bfc2fc176597
Peterkipkoechbii <peterpkoech@gmail.com>
2023-02-17 14:11:46 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
+
+/**
+ *  more headers goes there
+ * betty style doc for function main goes there 
+*/
+
+int main(void)
+
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+        printf("%d", i);
+	printf("\n");
+
+	return (0);
+}
