9c087d42b6bb2fec6cde8c081a4fe4eee75c4cf1
Peterkipkoechbii <peterpkoech@gmail.com>
2023-02-17 14:38:28 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
+
+/**
+ * more headers goes there
+ * betty style doc for function main goes there 
+*/
+
+int main(void)
+
+{
+	int d;
+	char low;
+
+	for (d = '0'; d <= '9'; d++);
+	putchar(d);
+
+	for (low = 'a'; low <= 'f'; low++);
+	putchar(low);
+	putchar('\n');
+
+	return (0);
+}
+	
