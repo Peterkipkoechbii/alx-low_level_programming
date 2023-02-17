fa7fa414468e899dc9e5b5c140ab229d79e524ac
Peterkipkoechbii <peterpkoech@gmail.com>
2023-02-17 14:31:58 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
+
+/**
+ * more headers goes there
+ * betty style doc for function main goes there 
+*/
+
+int main(void)
+
+{
+             char  low;
+
+	     for (low = 'z'; low >= 'a'; low--)
+             putchar(low);
+	     putchar('\n');
+
+	     return (0);
+}
