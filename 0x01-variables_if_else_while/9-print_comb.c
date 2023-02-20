56b0b4abb6647a95c333b3c5e62709b66862eeff
Peterkipkoechbii <peterpkoech@gmail.com>
2023-02-20 14:32:55 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,25 +5,24 @@
 #include <stdio.h>
 
 /**
- * more headers goes there
- * betty style doc for function main goes there 
+ * main - combination of single digit numbers
+ * Return: Always 0
 */
 
 int main(void)
-
 {
-	int d;
-	 
-	for (d = '0'; d <= '0'; d++)
-	{
-	putchar(d);
-	if (d !='9')
-	{
-	putchar(',');
-	putchar(' ');
-	}
-	}
-	putchar('\n');
+int d;
 
-	return (0);
+for (d = '0'; d <= '9'; d++)
+{
+putchar(d);
+if (d != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+
+return (0);
 }
