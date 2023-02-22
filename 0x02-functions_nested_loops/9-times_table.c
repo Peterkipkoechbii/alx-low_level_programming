e264c76ecbf56a56ee9bd06f9eee4cc2da1b7e2e
Peterkipkoechbii <peterpkoech@gmail.com>
2023-02-22 08:57:24 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,7 +9,7 @@ void times_table(void)
 {
 int x, y, z, u, d;
 
-for (x = 0; a <= 9; x++)
+for (x = 0; x <= 9; x++)
 {
 for (y = 0; y <= 9; y++)
 {
@@ -26,6 +26,7 @@ _putchar(u + '0');
 else
 {
 if (y != 0)
+{
 _putchar(44);
 _putchar(32);
 _putchar(32);
@@ -34,4 +35,5 @@ _putchar(z + '0');
 }
 }
 _putchar('\n');
+}
 }
