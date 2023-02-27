e590ddf11e4c6481e15cdca33224087fb301eb50
Peterkipkoechbii <peterpkoech@gmail.com>
2023-02-27 14:16:19 -0800

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,28 +7,28 @@
 */
 int _atoi(char *s)
 {
-int i;
+int c;
 unsigned int ni = 0;
 int min = 1;
 int isi = 0;
 
-while (s[i])
+while (s[c])
 {
-if (s[i] == 45)
+if (s[c] == 45)
 {
 min *= -1;
 }
-while (s[i] >= 48 && s[i] <= 57)
+while (s[c] >= 48 && s[c] <= 57)
 {
 isi = 1;
-ni = (ni * 10) + (s[i] - '0');
-i++;
+ni = (ni * 10) + (s[c] - '0');
+c++;
 }
 if (isi == 1)
 {
 break;
 }
-i++;
+c++;
 }
 ni *= min;
 return (ni);
